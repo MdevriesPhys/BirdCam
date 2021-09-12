80ef5b472c1ecb6d91c4931cc074a91a2834bca3
MdevriesPhys <mitchell@devries.ink>
2021-09-12 14:57:28 +1000

    flask is hard

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response
+import cv2
+import picamera as picamera
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  #read in a camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+#define template route
+@app.route('/')
+def index():
+    return render_template('page.html')
+
+#define route for video feed
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+#init flask
+app =Flask(__name__)
+#init camera, for rpi use picamera command
+#camera = cv2.VideoCapture(0)
+camera = picamera.PiCamera(resolution='640x480', framerate=24)
+if __name__ == "__main__":
+    app.run(debug=True)
