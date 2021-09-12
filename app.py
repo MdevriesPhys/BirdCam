8f0583ee2afa0679d2432bbc5fa8090a0460fb07
MdevriesPhys <mitchell@devries.ink>
2021-09-12 16:21:44 +1000

    getting rpi camera to work

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="192.168.8.108", port=8000,debug=True)
