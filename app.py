919fc9e7dea50e195668da93c855eeb323ff4b3b
MdevriesPhys <mitchell@devries.ink>
2021-09-12 17:18:56 +1000

    getting rpi camera to work

--- app.py
+++ app.py
@@ -12,16 +12,6 @@ class StreamingOutput(object):
         self.buffer = io.BytesIO()
         self.condition = Condition()
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
-
 def gen_frames():
     while True:
         global net, classNames
